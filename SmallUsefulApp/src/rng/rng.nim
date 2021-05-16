2da9aee567b574033b465e209a36be02936b9b40
pyautogui <rami-rosy@outlook.com>
2021-05-16 11:19:56 -0700

    Update

--- SmallUsefulApp/src/rng/rng.nim
+++ SmallUsefulApp/src/rng/rng.nim
@@ -15,93 +15,93 @@ from strutils import parseInt, inttoStr
 
 type CustomButton = ref object of Button
 method handleDrawEvent(control: CustomButton, event: DrawEvent) =
-  let canvas = event.control.canvas
-  canvas.areaColor = rgb(0, 255, 255)
-  canvas.textColor = rgb(55, 55, 55)
-  canvas.lineColor = rgb(255, 255, 255)
-  canvas.drawRectArea(0, 0, control.width, control.height)
-  canvas.drawTextCentered(control.text)
-  canvas.drawRectOutline(0, 0, control.width, control.height)
-  
+let canvas = event.control.canvas
+canvas.areaColor = rgb(0, 255, 255)
+canvas.textColor = rgb(55, 55, 55)
+canvas.lineColor = rgb(255, 255, 255)
+canvas.drawRectArea(0, 0, control.width, control.height)
+canvas.drawTextCentered(control.text)
+canvas.drawRectOutline(0, 0, control.width, control.height)
+
 proc newButton(text = ""): Button =
-  result = new CustomButton
-  result.init()
-  result.text = text
+result = new CustomButton
+result.init()
+result.text = text
+
+app.init()
+var w = newWindow("random")
+w.height = 1000
+w.width = 1000
+
+
+
+
+
+var container = newLayoutContainer(Layout_Vertical)
+var file1 = newLayoutContainer(Layout_Horizontal)
+var file2 = newLayoutContainer(Layout_Horizontal)
+var file3 = newLayoutContainer(Layout_Horizontal)
+var file4 = newLayoutContainer(Layout_Horizontal)
+var file5 = newLayoutContainer(Layout_Horizontal)
+var file6 = newLayoutContainer(Layout_Horizontal)
+
+w.add(container)
+file1.height = 5
+file2.height = 30
+file3.height = 30
+file4.height = 17
+file6.height = 15
+container.add(file1)
+container.add(file2)
+container.add(file3)
+container.add(file4)
+container.add(file5)
+container.add(file6)
+
+var quit = newButton("Quit")
+var button = newButton("Generate a random int between the two ints you inputed above. They have to be valid ints.")
+button.widthMode = WidthMode_Expand
+quit.widthMode = WidthMode_Expand
+file4.add(button)
+file1.add(quit)
+
+var text = newTextArea()
+text.editable = false
+text.fontSize = 48
+file5.add(text)
+
+
+var text1label = newButton("Small number on top, big number below.")
+text1label.widthMode = WidthMode_Expand
+
+file6.add(text1label)
+var text1 = newTextArea()
+text1.editable = true
+file2.add(text1)
+var text2 = newTextArea()
+file3.add(text2)
+text2.editable = true
+
+
+
+
+quit.onClick = proc(event: ClickEvent) =
+  app.quit()
+
+button.onClick = proc(event: ClickEvent) =
+  try:
+    var a = parseInt(text1.text) 
+    var b = parseInt(text2.text)
+    randomize()
+    text.text = intToStr((rand(a..b)))
+  except Exception:
+    app.quit()
+    app.run()
+
+if Key_Q.isDown() and Key_ControlL.isDown():
+  app.quit()
 
 proc run1*() =
-  app.init()
-  var w = newWindow("random")
-  w.height = 1000
-  w.width = 1000
-
-
-
-
-
-  var container = newLayoutContainer(Layout_Vertical)
-  var file1 = newLayoutContainer(Layout_Horizontal)
-  var file2 = newLayoutContainer(Layout_Horizontal)
-  var file3 = newLayoutContainer(Layout_Horizontal)
-  var file4 = newLayoutContainer(Layout_Horizontal)
-  var file5 = newLayoutContainer(Layout_Horizontal)
-  var file6 = newLayoutContainer(Layout_Horizontal)
-
-  w.add(container)
-  file1.height = 5
-  file2.height = 30
-  file3.height = 30
-  file4.height = 17
-  file6.height = 15
-  container.add(file1)
-  container.add(file2)
-  container.add(file3)
-  container.add(file4)
-  container.add(file5)
-  container.add(file6)
-
-  var quit = newButton("Quit")
-  var button = newButton("Generate a random int between the two ints you inputed above. They have to be valid ints.")
-  button.widthMode = WidthMode_Expand
-  quit.widthMode = WidthMode_Expand
-  file4.add(button)
-  file1.add(quit)
-
-  var text = newTextArea()
-  text.editable = false
-  text.fontSize = 48
-  file5.add(text)
-
-
-  var text1label = newButton("Small number on top, big number below.")
-  text1label.widthMode = WidthMode_Expand
-
-  file6.add(text1label)
-  var text1 = newTextArea()
-  text1.editable = true
-  file2.add(text1)
-  var text2 = newTextArea()
-  file3.add(text2)
-  text2.editable = true
-
-
-
-
-  quit.onClick = proc(event: ClickEvent) =
-    app.quit()
-
-  button.onClick = proc(event: ClickEvent) =
-    try:
-      var a = parseInt(text1.text) 
-      var b = parseInt(text2.text)
-      randomize()
-      text.text = intToStr((rand(a..b)))
-    except Exception:
-      app.quit()
-      app.run()
-
-  if Key_Q.isDown() and Key_ControlL.isDown():
-    app.quit()
   w.show()
   app.run()
-
 
