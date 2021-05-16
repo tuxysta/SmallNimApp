dbf474f94f561a9e9fa21ea65bb709270c14d31b
pyautogui <rami-rosy@outlook.com>
2021-05-15 19:01:21 -0700

    Optimize

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 #ASmallApp
 
 This is a fun little project I put together in my spare time, probably will revisit it later. I hope it is useful to someone at some point in time.
-This app can edit files, be used as a calculator, and act as a convenient rng. All in 1.3MB when compiled with optimization. Written on mac. Would probably take minimal changes to work on windows or linux. Might do that at some point.
+This app can act as a ceaser shifter, be used as a calculator, act as a convenient rng, and play a guessing game. All in under 5MB when compiled with optimization. Written on mac. Would probably take minimal changes to work on windows or linux. Might do that at some point.
 
 
 
--- SmallUsefulApp/src/SmallUsefulapp.nim
+++ SmallUsefulApp/src/SmallUsefulapp.nim
@@ -10,7 +10,7 @@
 #THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 from calc/calc import win
-import os, nigui, strutils, parseutils, math
+import nigui
 from  rng/rng import run1
 from NumberGuess/game import win
 from encrypttext/encrypttext import run1
@@ -39,7 +39,6 @@ window.width = 400
 
 var con = newLayoutContainer(Layout_Vertical)
 var middleCon = newLayoutContainer(Layout_Horizontal)
-
 var middleCon1 = newLayoutContainer(Layout_Horizontal)
 var middleCon2 = newLayoutContainer(Layout_Horizontal)
 
